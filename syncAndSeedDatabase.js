7fae439973bb249e0ebdc712e5b4204ed14b2fd3
Thiago Volpi Rabethge~ <thiago.rabethge@gmail.com>
2024-10-27 20:25:20 -0300

    refactor: sync and seed database

--- syncAndSeedDatabase.js
+++ syncAndSeedDatabase.js
@@ -1,15 +1,17 @@
 import sequelize from './database.js';
-import { seedMovies } from "./seeds/movieSeeds.js";
-import { seedSeries } from "./seeds/seriesSeeds.js";
-import { seedStreamings } from './seeds/streamingSeeds.js';
 import { seedEmotions } from './seeds/emotionSeeds.js';
+import { seedMovies } from './seeds/movieSeeds.js';
+import { seedSeries } from './seeds/seriesSeeds.js';
+import { seedStreamings } from './seeds/streamingSeeds.js';
 
 const syncAndSeedDatabase = async () => {
   try {
     await sequelize.sync({ force: true });
     console.log('Database synced.');
 
-    // Execute seed functions
+    await seedEmotions();
+    console.log('Emotions seeded.');
+
     await seedStreamings();
     console.log('Streamings seeded.');
 
@@ -18,9 +20,6 @@ const syncAndSeedDatabase = async () => {
 
     await seedSeries();
     console.log('Series seeded.');
-
-    await seedEmotions(); // Seed the Emotion model
-    console.log('Emotions seeded.');
 
   } catch (error) {
     console.error('Error syncing or seeding the database:', error);
